57d750a9650ccb5e2f0f55dd91e86e2672b9631c
Pep Carmona <pep.carmona.coll@gmail.com>
2023-09-12 15:25:21 +0200

    add bidding actions

---
+++ src/components/BiddingActions.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { AuctionUser } from '@/stores/selectedUser';
+import AppButton from './AppButton.vue';
+import AppNumberInput from './AppNumberInput.vue';
+import { computed, ref } from 'vue';
+
+interface BiddingActionsProps {
+  selectedUser: AuctionUser;
+}
+const props = defineProps<BiddingActionsProps>();
+
+interface BiddingActionsEmits {
+  (eventName: 'send-buyer-price', value: number): void;
+  (eventName: 'send-seller-price', value: number): void;
+}
+const emit = defineEmits<BiddingActionsEmits>();
+
+const amount = ref<number>();
+const isValidInput = ref<boolean>(false);
+
+const buyerInputLabel = 'Puja máxima';
+const sellerInputLabel = 'Precio mínimo';
+const moneyInputLabel = computed(() =>
+  props.selectedUser === AuctionUser.Buyer ? buyerInputLabel : sellerInputLabel
+);
+
+function emitAmount(): void {
+  if (amount.value === undefined) {
+    throw Error('Can not emit undefined amount');
+  }
+
+  switch (props.selectedUser) {
+    case AuctionUser.Buyer:
+      emit('send-buyer-price', amount.value);
+      break;
+
+    case AuctionUser.Seller:
+      emit('send-seller-price', amount.value);
+      break;
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="emitAmount">
+    <AppNumberInput
+      id="money-input"
+      :label="moneyInputLabel"
+      @input="amount = $event"
+      @check-validity="isValidInput = $event"
+    />
+    <AppButton type="submit" :aria-disabled="!isValidInput"> Enviar </AppButton>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/__tests__/BiddingActions.spec.ts
@@ -0,0 +1,76 @@
+import { describe, it, expect } from 'vitest';
+
+import { DOMWrapper, VueWrapper, mount } from '@vue/test-utils';
+import BiddingActions from '../BiddingActions.vue';
+import { AuctionUser } from '@/stores/selectedUser';
+import AppNumberInput from '../AppNumberInput.vue';
+import AppButton from '../AppButton.vue';
+
+describe('BiddingActions', () => {
+  const { innerButton, innerInput, wrapper } = createBiddingActionsWrapper();
+
+  it('displays correct input label depending on selectedUser prop value', async () => {
+    const buyerInputLabel = 'Puja máxima';
+    const sellerInputLabel = 'Precio mínimo';
+
+    expect(wrapper.html()).toContain(buyerInputLabel);
+
+    await wrapper.setProps({ selectedUser: AuctionUser.Seller });
+    expect(wrapper.html()).toContain(sellerInputLabel);
+  });
+
+  it('should have disabled submit button by default', () => {
+    expect(innerButton.element.getAttribute('aria-disabled')).toBe('true');
+  });
+
+  it('should have disabled button when input value is invalid', async () => {
+    await innerInput.setValue('a');
+    expect(innerButton.element.getAttribute('aria-disabled')).toBe('true');
+
+    await innerInput.setValue(-10);
+    expect(innerButton.element.getAttribute('aria-disabled')).toBe('true');
+  });
+
+  it('should have enabled button when input value is valid', async () => {
+    await innerInput.setValue(0);
+    expect(innerButton.element.getAttribute('aria-disabled')).toBe('false');
+
+    await innerInput.setValue(10);
+    expect(innerButton.element.getAttribute('aria-disabled')).toBe('false');
+  });
+
+  it('should emit correct action depending on selectedUser prop value', async () => {
+    const buyerBiddingEmit = 'send-buyer-price';
+    const sellerBiddingEmit = 'send-seller-price';
+    const { innerInput, wrapper } = createBiddingActionsWrapper();
+
+    await innerInput.setValue(10);
+    await wrapper.get('form').trigger('submit');
+    expect(wrapper.emitted(buyerBiddingEmit)).toHaveLength(1);
+
+    await wrapper.setProps({ selectedUser: AuctionUser.Seller });
+
+    await innerInput.setValue(10);
+    await wrapper.get('form').trigger('submit');
+    expect(wrapper.emitted(sellerBiddingEmit)).toHaveLength(1);
+  });
+});
+
+function createBiddingActionsWrapper(): {
+  wrapper: VueWrapper;
+  innerInput: Omit<DOMWrapper<HTMLInputElement>, 'exists'>;
+  innerButton: Omit<DOMWrapper<HTMLButtonElement>, 'exists'>;
+} {
+  const wrapper = mount(BiddingActions, {
+    props: { selectedUser: AuctionUser.Buyer }
+  });
+
+  const innerInput = wrapper.getComponent(AppNumberInput).get('input');
+  const innerButton = wrapper.getComponent(AppButton).get('button');
+
+  return {
+    wrapper,
+    innerInput,
+    innerButton
+  };
+}
